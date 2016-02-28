d080d9eef5737e8f97adb0f01caff69b3cd4be2c
wl <wlinios@sina.com>
2016-02-28 20:42:09 +0800

    first

--- HealthKitStudy/AppDelegate.m
+++ HealthKitStudy/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <HealthKit/HealthKit.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    
     return YES;
 }
 
